aac3a6e9b51ea1f6125d597699b88280e37c24c9
axzelcc <108249967+axzelcc@users.noreply.github.com>
2024-11-06 12:35:25 -0500

    fix commends

--- client.js
+++ client.js
@@ -1,36 +1,26 @@
-// websocket-client.js
 const io = require('socket.io-client');
 
-// Definir tus credenciales de autenticación básica
 const username = 'axzelcc';
 const password = '123';
 
-// Codificar las credenciales en base64
 const basicAuth = `Basic ${Buffer.from(username + ':' + password).toString('base64')}`;
 
-// Conectar al servidor WebSocket con autenticación básica
 const socket = io('http://localhost:3000', {
   extraHeaders: {
-    'Authorization': basicAuth // Pasar las credenciales en el encabezado
+    'Authorization': basicAuth
   }
 });
 
-// Escuchar eventos del servidor (por ejemplo, para recibir un mensaje)
 socket.on('Mensaje', (data) => {
   console.log('Mensaje recibido:', data);
 });
 
-// Enviar un mensaje al servidor WebSocket
 function sendMessage() {
   const message = {
-    content: 'Bien estamo activo', // El contenido del mensaje
-    userId: 1, // El ID del usuario que envía el mensaje
+    content: 'Bien estamo activo', 
+    userId: 1, 
   };
   socket.emit('mensaje', message );
 }
 
-// Llamar a la función para enviar un mensaje
 sendMessage();
-
-// Desconectar cuando ya no sea necesario
-//socket.disconnect();
